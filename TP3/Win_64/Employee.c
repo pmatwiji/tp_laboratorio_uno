668c5a3517e59a45364693591fedab90eadff3a1
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-11-05 12:13:23 -0300

    principio tp3

---
+++ TP3/Win_64/Employee.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Employee.h"
+
+int menu()
+{
+    int opcion;
+    fflush(stdin);
+    printf("MENU:\n"
+            "1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).\n"
+            "2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).\n"
+            "3. Alta de empleado\n"
+            "4. Modificar datos de empleado\n"
+            "5. Baja de empleado\n"
+            "6. Listar empleados\n"
+            "7. Ordenar empleados\n"
+            "8. Guardar los datos de los empleados en el archivo data.csv (modo texto).\n"
+            "9. Guardar los datos de los empleados en el archivo data.csv (modo binario).\n"
+            "10. Salir\n"
+           "Ingrese una opcion: ");
+    fflush(stdin);
+    scanf("%d", &opcion);
+
+    while(opcion < 0 || opcion > 10)
+    {
+        printf("Por favor, ingrese una opcion valida\n");
+        system("pause");
+        system("cls");
+        fflush(stdin);
+        printf("MENU:\n"
+            "1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).\n"
+            "2. Cargar los datos de los empleados desde el archivo data.csv (modo binario).\n"
+            "3. Alta de empleado\n"
+            "4. Modificar datos de empleado\n"
+            "5. Baja de empleado\n"
+            "6. Listar empleados\n"
+            "7. Ordenar empleados\n"
+            "8. Guardar los datos de los empleados en el archivo data.csv (modo texto).\n"
+            "9. Guardar los datos de los empleados en el archivo data.csv (modo binario).\n"
+            "10. Salir\n"
+           "Ingrese una opcion: ");
+        fflush(stdin);
+        scanf("%d", &opcion);
+    }
+
+    return opcion;
+}
+
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr)
+{
+    Employee* empleado;
+    empleado = malloc(sizeof(Employee));
+
+    if(empleado!=NULL)
+    {
+        empleado->id=idStr;
+        strcpy(empleado->nombre, nombreStr);
+        strcpy(empleado->horasTrabajadas, horasTrabajadasStr);
+    }
+
+    return empleado;
+}
---
+++ TP3/Win_64/parser.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+    char auxId[4];
+    char auxNombre[128];
+    char auxHorasTrabajadas[4];
+    char auxSueldo[5];
+
+
+    while(!feof(pFile))
+    {
+        fscanf(pFile, "%[^,],%[^,],%[^,],%[^\n]\n", auxId, auxNombre, auxHorasTrabajadas, auxSueldo);
+        puts(auxId);
+    }
+
+
+
+
+    return 1;
+}
+
+/** \brief Parsea los datos los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee)
+{
+
+    return 1;
+}
